c9e79bd906c7e0d3e1b81f06e9d4643a87f0c97a
somereniptv-beep <somereniptv@gmail.com>
2025-11-04 22:01:10 +0100

    Update items.ts

--- data/items.ts
+++ data/items.ts
@@ -1,1 +1,5 @@
-
+export const ITEMS = [
+  { id: '1', name: 'Fix Laptop - $50', price: 5000 },
+  { id: '2', name: 'Print 100 Flyers - $30', price: 3000 },
+  { id: '3', name: 'Make Logo - $80', price: 8000 }
+];
